46430626f2f8e80161990d5f64ad007d1468c7a7
chunie <1443232120@qq.com>
2022-05-31 22:15:59 +0800

    Add files via upload

---
+++ omnioperator/omniop-spark-extension/cpp/src/jni/OrcColumnarBatchJniReader.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
+ */
+
+/* Header for class THESTRAL_PLUGIN_ORCCOLUMNARBATCHJNIREADER_H */
+
+#ifndef THESTRAL_PLUGIN_ORCCOLUMNARBATCHJNIREADER_H
+#define THESTRAL_PLUGIN_ORCCOLUMNARBATCHJNIREADER_H
+
+#include "orc/ColumnPrinter.hh"
+#include "orc/Exceptions.h"
+#include "orc/Type.hh"
+#include "orc/Vector.hh"
+#include "orc/Reader.hh"
+#include "orc/OrcFile.hh"
+#include "orc/MemoryPool.hh"
+#include "orc/sargs/SearchArgument.hh"
+#include "orc/sargs/Literal.hh"
+#include <getopt.h>
+#include <string>
+#include <memory>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include "jni.h"
+#include "json/json.h"
+#include "vector/vector_common.h"
+#include "util/omni_exception.h"
+#include <time.h>
+#include <sstream>
+#include "../common/debug.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum class Operator {
+    OR,
+    AND,
+    NOT,
+    LEAF,
+    CONSTANT
+};
+
+enum class PredicateOperatorType {
+    EQUALS = 0,
+    NULL_SAFE_EQUALS,
+    LESS_THAN,
+    LESS_THAN_EQUALS, IN, BETWEEN, IS_NULL
+};
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      initializeReader
+ * Signature:   (Ljava/lang/String;Lorg/json/simple/JSONObject;)J
+ */
+JNIEXPORT jlong JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_initializeReader
+        (JNIEnv* env, jobject jObj);
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      initializeRecordReader
+ * Signature:   (JLorg/json/simple/JSONObject;)J
+ */
+JNIEXPORT jlong JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_initializeRecordReader
+        (JNIEnv* env, jobject jObj, jlong reader, jobject job);
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      initializeRecordReader
+ * Signature:   (JLorg/json/simple/JSONObject;)J
+ */
+JNIEXPORT jlong JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_initializeBatch
+        (JNIEnv* env, jobject jObj, jlong rowReader, jlong batchsize);
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      recordReaderNext
+ * Signature:   (J[I[J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_recordReaderNext
+        (JNIEnv *, jobject, jlong, jlong, jlong, jintArray, jlongArray);
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      recordReaderGetRowNumber
+ * Signature:   (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_recordReaderGetRowNumber
+        (JNIEnv *, jobject, jlong);
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      recordReaderGetProgress
+ * Signature:   (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_recordReaderGetProgress
+        (JNIEnv *, jobject, jlong);
+
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      recordReaderClose
+ * Signature:   (J)F
+ */
+JNIEXPORT void JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_recordReaderClose
+        (JNIEnv *, jobject, jlong, jlong, jlong);
+
+/*
+ * Class:       come_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader
+ * Method:      recordReaderSeekToRow
+ * Signature:   (JJ)F
+ */
+JNIEXPORT void JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_recordReaderSeekToRow
+        (JNIEnv *, jobject, jlong, jlong);
+
+JNIEXPORT jobjectArray JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_getAllColumnNames
+        (JNIEnv *, jobject, jlong);
+
+JNIEXPORT jlong JNICALL Java_com_huawei_boostkit_spark_jni_OrcColumnarBatchJniReader_getNumberOfRows(JNIEnv *env,
+        jobject jObj, jlong rowReader, jlong batch);
+
+int getLiteral(orc::Literal &lit, int leafType, std::string value);      
+
+int buildLeafs(int leafOp, std::vector<orc::Literal> &litList, orc::Literal &lit, std::string leafNameString, int leafType,
+    orc::searchArgumentBuilder &builder);
+
+int copyToOminVec(int maxLen, int vcType, int &ominTypeId, uint64_t &ominVecId, orc::ColumnVectorBatch *field);  
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ omnioperator/omniop-spark-extension/cpp/src/jni/jni_common.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
+ */
+
+#ifdef THESTRAL_PLUGIN_MASTER_JNI_COMMON_H
+#define THESTRAL_PLUGIN_MASTER_JNI_COMMON_H
+
+#include <jni.h>
+
+#include "../common/common.h"
+
+static jclass illegal_access_exception_class;
+
+spark::CompressionKind GetCompressionType(JNIEnv* env, jstring codec_jstr) {
+    auto codec_c = env->GetStringUTFChars(codec_jstr, JNI_FALSE);
+    auto codec = std::string(codec_c);
+    auto compression_type = GetCompressionType(codec);
+    env->ReleaseStringUTFChars(codec_jstr, codec_c);
+    return compression_type;
+}
+
+jclass CreateGlobalClassReference(JNIEnv* env, const char* class_name) {
+    jclass local_class = env->FindClass(class_name);
+    jclass global_class = (jclass)env->NewGlobalRef(local_class);
+    env->DeleteLocalRef(local_class);
+    if (global_class == nullptr) {
+        std::string error_message = "unable to createGlobalClassReference for" + std::string(class_name);
+        env->ThrowNew(illegal_access_exception_class, error_message.c_str());
+    }
+    return global_class;
+}
+
+jmethodID GetMethodID() {
+    jmethodID ret = env->GetMethodID(this_class, name, sig);
+    if (ret == nullptr) {
+        std::string error_message = "Unable to find method " + std::string(name) + " within signature" + std::string(sig);
+        env->ThrowNew(illegal_access_exception_class, error_message.c_str());
+    }
+
+    return ret;
+}
+#endif //THESTRAL_PLUGIN_MASTER_JNI_COMMON_H
