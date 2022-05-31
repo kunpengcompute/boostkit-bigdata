f51bcdd3c013a0c6ebdf0ea9eefa9d64bf5ff2b9
reflectt6 <reflectt6@gmail.com>
2022-05-31 10:38:53 +0800

    replace code license with APACHE LISENCE-2.0.

--- omnidata/omnidata-spark-connector/connector/src/main/scala/org/apache/spark/sql/execution/ndp/NdpPushDown.scala
+++ omnidata/omnidata-spark-connector/connector/src/main/scala/org/apache/spark/sql/execution/ndp/NdpPushDown.scala
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
