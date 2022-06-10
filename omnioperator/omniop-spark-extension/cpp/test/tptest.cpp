befb035c80b69e4b84fcc9be0d48fbe8ab818264
chenguang <18767185082@163.com>
2022-06-10 17:39:27 +0800

    cpp test file add

---
+++ omnioperator/omniop-spark-extension/cpp/test/tptest.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
+ */
+
+#include "gtest/gtest.h"
+
+int main()
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
