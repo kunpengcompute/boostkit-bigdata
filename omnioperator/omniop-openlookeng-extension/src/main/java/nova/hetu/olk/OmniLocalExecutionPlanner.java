a7d82dca1028393e90279422c851a6061c579cab
zhousipei <zsp233@qq.com>
2022-07-12 11:43:41 +0800

    remove unused import

--- omnioperator/omniop-openlookeng-extension/src/main/java/nova/hetu/olk/OmniLocalExecutionPlanner.java
+++ omnioperator/omniop-openlookeng-extension/src/main/java/nova/hetu/olk/OmniLocalExecutionPlanner.java
@@ -156,7 +156,6 @@ import nova.hetu.omniruntime.type.DataType;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
