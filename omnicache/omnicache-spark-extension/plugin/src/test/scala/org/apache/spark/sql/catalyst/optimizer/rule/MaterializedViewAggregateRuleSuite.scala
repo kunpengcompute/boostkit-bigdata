4efd7f33693b54a80f9fd4cd9d6e7db0b496e44c
helloxteen <seetoyou@vip.qq.com>
2022-08-21 23:12:10 +0800

    add some mv ut code

--- omnicache/omnicache-spark-extension/plugin/src/test/scala/org/apache/spark/sql/catalyst/optimizer/rule/MaterializedViewAggregateRuleSuite.scala
+++ omnicache/omnicache-spark-extension/plugin/src/test/scala/org/apache/spark/sql/catalyst/optimizer/rule/MaterializedViewAggregateRuleSuite.scala
@@ -19,4 +19,225 @@ package org.apache.spark.sql.catalyst.optimizer.rule
 
 class MaterializedViewAggregateRuleSuite extends RewriteSuite {
 
+  test("mv_agg1") {
+    spark.sql(
+      """
+        |DROP MATERIALIZED VIEW IF EXISTS mv_agg1;
+        |""".stripMargin
+    )
+    spark.sql(
+      """
+        |CREATE MATERIALIZED VIEW IF NOT EXISTS mv_agg1
+        |AS
+        |SELECT c.empid,c.deptno,c.locationid,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg
+        |FROM column_type c
+        |GROUP BY c.empid,c.deptno,c.locationid;
+        |""".stripMargin
+    )
+  }
+
+  test("mv_agg1_1") {
+    // group by column,agg is same to view,additional agg on view column
+    val sql =
+      """
+        |SELECT c.empid,c.deptno,c.locationid,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg,
+        |max(c.deptno) as _max2,min(c.deptno) as _min2,
+        |count(c.deptno) as _count2,count(distinct c.deptno) as _count_distinct2,
+        |avg(c.deptno) as _avg2
+        |FROM column_type c
+        |GROUP BY c.empid,c.deptno,c.locationid;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_agg1", noData = false)
+  }
+
+  test("mv_agg1_2") {
+    // group by column is same to view,additional agg on column not in view
+    val sql =
+      """
+        |SELECT c.empid,c.deptno,c.locationid,sum(c.bytetype) as _sum,
+        |FROM column_type c
+        |GROUP BY c.empid,c.deptno,c.locationid;
+        |""".stripMargin
+    compareNotRewriteAndRows(sql, noData = false)
+  }
+
+  test("mv_agg1_disable") {
+    val sql = "ALTER MATERIALIZED VIEW mv_agg1 DISABLE REWRITE;"
+    spark.sql(sql).show()
+  }
+
+  test("mv_agg2") {
+    spark.sql(
+      """
+        |DROP MATERIALIZED VIEW IF EXISTS mv_agg2;
+        |""".stripMargin
+    )
+    spark.sql(
+      """
+        |CREATE MATERIALIZED VIEW IF NOT EXISTS mv_agg2
+        |AS
+        |SELECT sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg
+        |FROM column_type c;
+        |""".stripMargin
+    )
+  }
+
+  test("mv_agg2_1") {
+    // group by column(is empty),agg is same to view
+    val sql =
+      """
+        |SELECT sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg
+        |FROM column_type c;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_agg2", noData = false)
+  }
+
+  test("mv_agg2_disable") {
+    val sql = "ALTER MATERIALIZED VIEW mv_agg2 DISABLE REWRITE;"
+    spark.sql(sql).show()
+  }
+
+  test("mv_agg3") {
+    spark.sql(
+      """
+        |DROP MATERIALIZED VIEW IF EXISTS mv_agg3;
+        |""".stripMargin
+    )
+    spark.sql(
+      """
+        |CREATE MATERIALIZED VIEW IF NOT EXISTS mv_agg3
+        |AS
+        |SELECT c.empid,c.deptno,c.locationid,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg
+        |FROM column_type c
+        |GROUP BY c.empid,c.deptno,c.locationid;
+        |""".stripMargin
+    )
+  }
+
+  test("mv_agg3_1") {
+    // group by column,agg is subset of view,additional agg on view column
+    val sql =
+      """
+        |SELECT c.empid,c.deptno,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,
+        |max(c.deptno) as _max2,min(c.deptno) as _min2,
+        |count(c.deptno) as _count2,count(distinct c.deptno) as _count_distinct2,
+        |avg(c.deptno) as _avg2
+        |FROM column_type c
+        |GROUP BY c.empid,c.deptno;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_agg3", noData = false)
+  }
+
+  test("mv_agg3_2") {
+    // group by column is subset of view,using non-rollup agg
+    val sql =
+      """
+        |SELECT c.empid,c.deptno,avg(c.decimaltype) as _avg
+        |FROM column_type c
+        |GROUP BY c.empid,c.deptno;
+        |""".stripMargin
+    compareNotRewriteAndRows(sql, noData = false)
+  }
+
+  test("mv_agg3_3") {
+    // group by column(is empty) is subset of view,,additional agg on view column
+    val sql =
+      """
+        |SELECT sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,
+        |max(c.deptno) as _max2,min(c.deptno) as _min2,
+        |count(c.deptno) as _count2,count(distinct c.deptno) as _count_distinct2,
+        |avg(c.deptno) as _avg2
+        |FROM column_type c ;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_agg3", noData = false)
+  }
+
+  test("mv_agg3_disable") {
+    val sql = "ALTER MATERIALIZED VIEW mv_agg3 DISABLE REWRITE;"
+    spark.sql(sql).show()
+  }
+
+
+  test("mv_agg4") {
+    spark.sql(
+      """
+        |DROP MATERIALIZED VIEW IF EXISTS mv_agg4;
+        |""".stripMargin
+    )
+    spark.sql(
+      """
+        |CREATE MATERIALIZED VIEW IF NOT EXISTS mv_agg4
+        |AS
+        |SELECT c.empid,c.deptno,c.locationid,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg
+        |FROM column_type c JOIN emps e
+        |ON c.empid=e.empid
+        |AND c.empid=1
+        |GROUP BY c.empid,c.deptno,c.locationid;
+        |""".stripMargin
+    )
+  }
+
+  test("mv_agg4_1") {
+    // group by column,agg is same to view,additional agg on view column
+    val sql =
+      """
+        |SELECT c.empid,c.deptno,c.locationid,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,count(distinct c.datetype) as _count_distinct,
+        |avg(c.decimaltype) as _avg,
+        |max(c.deptno) as _max2,min(c.deptno) as _min2,
+        |count(c.deptno) as _count2,count(distinct c.deptno) as _count_distinct2,
+        |avg(c.deptno) as _avg2
+        |FROM column_type c JOIN emps e
+        |ON c.empid=e.empid
+        |AND c.empid=1
+        |GROUP BY c.empid,c.deptno,c.locationid;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_agg4", noData = false)
+  }
+
+  test("mv_agg4_2") {
+    // group by column,agg is subset of view,additional agg on view column
+    val sql =
+      """
+        |SELECT c.empid,c.deptno,sum(c.integertype) as _sum,
+        |max(c.longtype) as _max,min(c.floattype) as _min,
+        |count(c.doubletype) as _count,
+        |max(c.deptno) as _max2,min(c.deptno) as _min2,
+        |count(c.deptno) as _count2,count(distinct c.deptno) as _count_distinct2,
+        |avg(c.deptno) as _avg2
+        |FROM column_type c JOIN emps e
+        |ON c.empid=e.empid
+        |AND c.empid=1
+        |GROUP BY c.empid,c.deptno;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_agg4", noData = false)
+  }
+
+  test("mv_agg4_disable") {
+    val sql = "ALTER MATERIALIZED VIEW mv_agg4 DISABLE REWRITE;"
+    spark.sql(sql).show()
+  }
 }
--- omnicache/omnicache-spark-extension/plugin/src/test/scala/org/apache/spark/sql/catalyst/optimizer/rule/MaterializedViewJoinRuleSuite.scala
+++ omnicache/omnicache-spark-extension/plugin/src/test/scala/org/apache/spark/sql/catalyst/optimizer/rule/MaterializedViewJoinRuleSuite.scala
@@ -19,4 +19,146 @@ package org.apache.spark.sql.catalyst.optimizer.rule
 
 class MaterializedViewJoinRuleSuite extends RewriteSuite {
 
+  test("mv_join1") {
+    spark.sql(
+      """
+        |DROP MATERIALIZED VIEW IF EXISTS mv_join1;
+        |""".stripMargin
+    )
+    spark.sql(
+      """
+        |CREATE MATERIALIZED VIEW IF NOT EXISTS mv_join1
+        |AS
+        |SELECT e.*,d.deptname
+        |FROM emps e JOIN depts d
+        |ON e.deptno=d.deptno;
+        |""".stripMargin
+    )
+  }
+
+  test("mv_join1_1") {
+    // is same to view
+    val sql =
+      """
+        |SELECT e.*,d.deptname
+        |FROM emps e JOIN depts d
+        |ON e.deptno=d.deptno;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_2") {
+    // is same to view, join order different
+    val sql =
+      """
+        |SELECT e.*,d.deptname
+        |FROM depts d JOIN emps e
+        |ON e.deptno=d.deptno;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_3") {
+    // view tables is subset of query
+    val sql =
+      """
+        |SELECT e.*,d.deptname,l.locationid
+        |FROM emps e JOIN depts d JOIN locations l
+        |ON e.deptno=d.deptno AND e.locationid=l.locationid;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_4") {
+    // view tables is subset of query, join order different
+    val sql =
+      """
+        |SELECT e.*,d.deptname,l.locationid
+        |FROM depts d JOIN locations l JOIN emps e
+        |ON e.deptno=d.deptno AND e.locationid=l.locationid;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_5") {
+    // view tables is subset of query, join with subquery
+    val sql =
+      """
+        |SELECT v1.*,l.locationid
+        |(SELECT e.*,d.deptname
+        |FROM emps e JOIN depts d
+        |ON e.deptno=d.deptno
+        |) v1
+        |JOIN locations l
+        |ON v1.locationid=l.locationid;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_6") {
+    // view tables is subset of query, join with subquery2
+    val sql =
+      """
+        |SELECT v1.*,d.deptname
+        |(SELECT e.*,l.locationid
+        |FROM emps e JOIN locations l
+        |ON e.locationid=l.locationid
+        |) v1
+        |JOIN depts d
+        |ON v1.deptno=d.deptno;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_7") {
+    // view tables is same to query, equal columns
+    val sql =
+      """
+        |SELECT d.deptno
+        |FROM emps e JOIN depts d
+        |ON e.deptno=d.deptno;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join1", noData = false)
+  }
+
+  test("mv_join1_disable") {
+    val sql = "ALTER MATERIALIZED VIEW mv_join1 DISABLE REWRITE;"
+    spark.sql(sql).show()
+  }
+
+  test("mv_join1") {
+    spark.sql(
+      """
+        |DROP MATERIALIZED VIEW IF EXISTS mv_join2;
+        |""".stripMargin
+    )
+    spark.sql(
+      """
+        |CREATE MATERIALIZED VIEW IF NOT EXISTS mv_join2
+        |AS
+        |SELECT e.*,c1.deptname
+        |FROM emps e JOIN column_type c1
+        |ON e.deptno=c1.deptno
+        |AND c1.deptno=1;
+        |""".stripMargin
+    )
+  }
+
+  test("mv_join2_1") {
+    // view tables is same to query, equal tables
+    val sql =
+      """
+        |SELECT e.*,c1.deptname
+        |FROM emps e JOIN column_type c1 JOIN column_type c2
+        |ON e.deptno=c1.deptno AND e.deptno=c2.deptno
+        |AND c1.deptno!=2
+        |AND c2.deptno=1;
+        |""".stripMargin
+    comparePlansAndRows(sql, "default", "mv_join2", noData = false)
+  }
+
+  test("mv_join2_disable") {
+    val sql = "ALTER MATERIALIZED VIEW mv_join2 DISABLE REWRITE;"
+    spark.sql(sql).show()
+  }
 }
