08d3d1582ff97d947b978dc981873fcff2f6a2c2
buddha23 <8994556+buddha23@user.noreply.gitee.com>
2022-10-25 03:49:43 +0000

    add omnioperator/omniop-spark-extension-ock/cpp/test/tptest.cpp.
    Signed-off-by: buddha23 <8994556+buddha23@user.noreply.gitee.com>

---
+++ omnioperator/omniop-spark-extension-ock/cpp/test/tptest.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2020-2022. Huawei Technologies Co., Ltd. All rights reserved.
+ */
+ 
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
