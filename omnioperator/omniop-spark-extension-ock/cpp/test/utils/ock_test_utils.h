5589825faec46da74b1ab587b1e9891667b95e01
buddha23 <8994556+buddha23@user.noreply.gitee.com>
2023-07-18 02:05:24 +0000

    update omnioperator/omniop-spark-extension-ock/cpp/test/utils/ock_test_utils.h.
    Signed-off-by: buddha23 <8994556+buddha23@user.noreply.gitee.com>

--- omnioperator/omniop-spark-extension-ock/cpp/test/utils/ock_test_utils.h
+++ omnioperator/omniop-spark-extension-ock/cpp/test/utils/ock_test_utils.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
-
+#include <type/data_types.h>
 #include "../../src/jni/concurrent_map.h"
 #define private public
 static const int varcharType = 5;
@@ -22,29 +22,29 @@ static ock::dopspark::ConcurrentMap<std::shared_ptr<ock::dopspark::OckSplitter>>
 
 static std::string Ocks_shuffle_tests_dir = "/tmp/OckshuffleTests";
 
-VectorBatch *OckCreateInputData(const int32_t numRows, const int32_t numCols, int32_t *inputTypeIds, int64_t *allData);
+std::unique_ptr<BaseVector> CreateVector(DataType &dataType, int32_t rowCount, va_list &args);
 
-Vector *OckbuildVector(const DataType &aggType, int32_t rowNumber);
+VectorBatch *OckCreateInputData(const DataTypes &types, int32_t rowCount, ...);
 
-Vector *OckNewbuildVector(const DataTypeId &typeId, int32_t rowNumber);
+VectorBatch *OckCreateVectorBatch(const DataTypes &types, int32_t rowCount, ...);
+
+BaseVector *OckNewbuildVector(const DataTypeId &typeId, int32_t rowNumber);
+
+VectorBatch *OckCreateVectorBatch_4varcharCols_withPid(int parNum, int rowNum);
 
 VectorBatch *OckCreateVectorBatch_1row_varchar_withPid(int pid, const std::string &inputChar);
 
 VectorBatch *OckCreateVectorBatch_4col_withPid(int parNum, int rowNum);
 
-VectorBatch *OckCreateVectorBatch_1longCol_withPid(int parNum, int rowNum);
-
 VectorBatch *OckCreateVectorBatch_2column_1row_withPid(int pid, std::string strVar, int intVar);
 
-VectorBatch *OckCreateVectorBatch_4varcharCols_withPid(int parNum, int rowNum);
+VectorBatch *OckCreateVectorBatch_5fixedCols_withPid(int parNum, int rowNum);
 
-VectorBatch *OckCreateVectorBatch_3fixedCols_withPid(int parNum, int rowNum);
-
-VectorBatch *OckCreateVectorBatch_1fixedCols_withPid(int parNum, int32_t rowNum);
+VectorBatch *OckCreateVectorBatch_1fixedCols_withPid(int parNum, int32_t rowNum, DataTypePtr fixColType);
 
 VectorBatch *OckCreateVectorBatch_2dictionaryCols_withPid(int partitionNum);
 
-VectorBatch *OckCreateVectorBatch_1decimal128Col_withPid(int partitionNum);
+VectorBatch *OckCreateVectorBatch_1decimal128Col_withPid(int partitionNum, int rowNum);
 
 VectorBatch *OckCreateVectorBatch_1decimal64Col_withPid(int partitionNum, int rowNum);
 
@@ -66,6 +66,52 @@ int OckTest_splitter_split(long splitter_id, VectorBatch *vb);
 void OckTest_splitter_stop(long splitter_id);
 
 void OckTest_splitter_close(long splitter_id);
+
+template <typename T> std::unique_ptr<BaseVector> CreateVector(int32_t length, T *values)
+{
+    std::unique_ptr<Vector<T>> vector = std::make_unique<Vector<T>>(length);
+    for (int32_t i = 0; i < length; i++) {
+        vector->SetValue(i, values[i]);
+    }
+    return vector;
+}
+
+template <omniruntime::type::DataTypeId typeId>
+std::unique_ptr<BaseVector> CreateFlatVector(int32_t length, va_list &args)
+{
+    using namespace omniruntime::type;
+    using T = typename NativeType<typeId>::type;
+    using VarcharVector = Vector<LargeStringContainer<std::string_view>>;
+    if constexpr (std::is_same_v<T, std::string_view> || std::is_same_v<T, uint8_t>) {
+        std::unique_ptr<VarcharVector> vector = std::make_unique<VarcharVector>(length);
+        std::string *str = va_arg(args, std::string *);
+        for (int32_t i = 0; i < length; i++) {
+            std::string_view value(str[i].data(), str[i].length());
+            vector->SetValue(i, value);
+        }
+        return vector;
+    } else {
+        std::unique_ptr<Vector<T>> vector = std::make_unique<Vector<T>>(length);
+        T *value = va_arg(args, T *);
+        for (int32_t i = 0; i < length; i++) {
+            vector->SetValue(i, value[i]);
+        }
+        return vector;
+    }
+}
+
+template <DataTypeId typeId>
+std::unique_ptr<BaseVector> CreateDictionary(BaseVector *vector, int32_t *ids, int32_t size)
+{
+    using T = typename NativeType<typeId>::type;
+    if constexpr (std::is_same_v<T, std::string_view> || std::is_same_v<T, uint8_t>) {
+        return VectorHelper::CreateStringDictionary(ids, size,
+            reinterpret_cast<Vector<LargeStringContainer<std::string_view>> *>(vector));
+    }
+    return VectorHelper::CreateDictionary(ids, size, reinterpret_cast<Vector<T> *>(vector));
+}
+
+
 
 template <typename T, typename V> T *OckCreateVector(V *values, int32_t length)
 {
