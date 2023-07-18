d48891f94ac84f54ce16023f1a388ff08e4efd59
buddha23 <8994556+buddha23@user.noreply.gitee.com>
2023-07-18 06:41:05 +0000

    move
    Signed-off-by: buddha23 <8994556+buddha23@user.noreply.gitee.com>

---
+++ omnioperator/omniop-spark-extension-ock/ock-omniop-shuffle/cpp/src/shuffle/ock_vector.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+#ifndef SPARK_THESTRAL_PLUGIN_OCK_VECTOR_H
+#define SPARK_THESTRAL_PLUGIN_OCK_VECTOR_H
+
+#include <vector/vector_common.h>
+
+namespace ock {
+namespace dopspark {
+class OckVector {
+public:
+    OckVector() = default;
+    ~OckVector() = default;
+
+    [[nodiscard]] inline uint32_t GetSize() const
+    {
+        return size;
+    }
+
+    void SetSize(uint32_t newSize)
+    {
+        this->size = newSize;
+    }
+
+    [[nodiscard]] inline uint32_t GetCapacityInBytes() const
+    {
+        return capacityInBytes;
+    }
+
+    void SetCapacityInBytes(uint32_t capacity)
+    {
+        capacityInBytes = capacity;
+    }
+
+    [[nodiscard]] inline void *GetValueNulls() const
+    {
+        return valueNullsAddress;
+    }
+
+    void SetValueNulls(void *address)
+    {
+        valueNullsAddress = address;
+    }
+
+    [[nodiscard]] inline void *GetValues() const
+    {
+        return valuesAddress;
+    }
+
+    void SetValues(void *address)
+    {
+        valuesAddress = address;
+    }
+
+    [[nodiscard]] inline void *GetValueOffsets() const
+    {
+        return valueOffsetsAddress;
+    }
+
+    int GetValueOffset(int index)
+    {
+        return static_cast<int32_t *>(valueOffsetsAddress)[index];
+    }
+
+    void SetValueOffsets(void *address)
+    {
+        valueOffsetsAddress = address;
+    }
+
+    inline void SetNextVector(std::shared_ptr<OckVector> next)
+    {
+        mNext = next;
+    }
+
+    inline std::shared_ptr<OckVector> GetNextVector()
+    {
+        return mNext;
+    }
+
+private:
+    uint32_t size = 0;
+    uint32_t capacityInBytes = 0;
+
+    void *valuesAddress = nullptr;
+    void *valueNullsAddress = nullptr;
+    void *valueOffsetsAddress = nullptr;
+
+    std::shared_ptr<OckVector> mNext = nullptr;
+};
+using OckVectorPtr = std::shared_ptr<OckVector>;
+}
+}
+#endif // SPARK_THESTRAL_PLUGIN_OCK_VECTOR_H
